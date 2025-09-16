ee69b5cc7702954574c01c31a818215aef16e2a7
hyein89 <bihontai@gmail.com>
2025-09-17 02:54:28 +0700

    Update 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../styles/404.module.css";
+import styles from "../styles/module.css";
 
 export default function NotFound() {
   return (
