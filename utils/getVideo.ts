21e8218edf02fe2afea5107df632b82770146b60
hyein89 <bihontai@gmail.com>
2025-09-17 00:50:13 +0700

    Update getVideo.ts

--- utils/getVideo.ts
+++ utils/getVideo.ts
@@ -1,9 +1,13 @@
-import data from "../public/videos.json";
-
-export function getVideoById(id: string) {
-  return (data as any[]).find((v) => v.id_video === id);
-}
+import raw from "../public/videos.json";
 
 export function getAllVideos() {
-  return data as any[];
+  return (raw as any[]).map((v) => ({
+    ...v,
+    id: v.id_video, // normalisasi id_video → id
+  }));
 }
+
+export function getVideoById(id: string) {
+  return getAllVideos().find((v) => v.id === id);
+}
+
