51e3d78027c0c751edde9718ac204362b0cd4515
hyein89 <bihontai@gmail.com>
2025-09-17 18:45:57 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,63 +1,11 @@
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
-import "../styles/globals.css";
-import Script from "next/script";
+import Layout from "../components/Layout"; // sesuaikan path
 
+import "../styles/globals.css"; // css global lu
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      {/* Google Analytics */}
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
-      />
-      <Script
-        id="ga-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XXXXXXX');
-          `,
-        }}
-      />
-
-      {/* Histats */}
-      <Script
-        id="histats"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            var _Hasync= _Hasync|| [];
-            _Hasync.push(['Histats.start', '1,4828760,4,0,0,0,00000000']);
-            _Hasync.push(['Histats.fasi', '1']);
-            _Hasync.push(['Histats.track_hits', '']);
-            (function() {
-              var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-              hs.src = ('//s10.histats.com/js15_as.js');
-              (document.head || document.body).appendChild(hs);
-            })();
-          `,
-        }}
-      />
-
-      {/* Layout + Content */}
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-
-      {/* Histats noscript fallback */}
-      <noscript>
-        <a href="https://www.histats.com" target="_blank" rel="noreferrer">
-          <img
-            src="//sstatic1.histats.com/0.gif?4828760&101"
-            alt="histats"
-            border="0"
-          />
-        </a>
-      </noscript>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
