3dddf0bb1c863e8df2982e46c0b830ec82e4d346
hyein89 <bihontai@gmail.com>
2025-09-18 00:08:23 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+// pages/_app.tsx
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
@@ -6,7 +7,7 @@ import Script from "next/script";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google Analytics - ganti G-XXXXXXX dengan GA Measurement ID kamu */}
       <Script
         strategy="afterInteractive"
         src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"
@@ -24,7 +25,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         }}
       />
 
-      {/* Histats */}
+      {/* Histats - ganti ID sesuai akunmu */}
       <Script
         id="histats"
         strategy="afterInteractive"
@@ -36,29 +37,28 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             _Hasync.push(['Histats.track_hits', '']);
             (function() {
               var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-              hs.src = ('//s10.histats.com/js15_as.js');
+              hs.src = ('https://s10.histats.com/js15_as.js');
               (document.head || document.body).appendChild(hs);
             })();
           `,
         }}
       />
 
-      {/* Layout + Content */}
+      {/* App layout */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
 
-      {/* Histats noscript fallback */}
+      {/* Histats noscript fallback (gambar tanpa border via style) */}
       <noscript>
         <a href="https://www.histats.com" target="_blank" rel="noreferrer">
           <img
-            src="//sstatic1.histats.com/0.gif?4828760&101"
+            src="https://sstatic1.histats.com/0.gif?4828760&101"
             alt="histats"
-            border="0"
+            style={{ border: 0 }}
           />
         </a>
       </noscript>
     </>
   );
 }
-
